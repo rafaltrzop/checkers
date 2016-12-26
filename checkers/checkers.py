48df3eb5207c92fa6e2074dbf239f6f9a00ec72e
Rafał Trzop <rafaltrzop@gmail.com>
2016-12-26 02:40:16 +0100

    Add default board state

--- checkers/checkers.py
+++ checkers/checkers.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask import render_template
+from .game import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    board = Gameboard().board
+    default_board = [
+        [WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece()],
+        [DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE],
+        [WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece(), WHITE_SQUARE, DarkPiece()],
+        [BLACK_SQUARE, WHITE_SQUARE, BLACK_SQUARE, WHITE_SQUARE, BLACK_SQUARE, WHITE_SQUARE, BLACK_SQUARE, WHITE_SQUARE],
+        [WHITE_SQUARE, BLACK_SQUARE, WHITE_SQUARE, BLACK_SQUARE, WHITE_SQUARE, BLACK_SQUARE, WHITE_SQUARE, BLACK_SQUARE],
+        [LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE],
+        [WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece()],
+        [LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE, LightPiece(), WHITE_SQUARE]
+    ]
+    board = Gameboard(default_board).board
     return render_template('index.html', board=board)
