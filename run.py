0fcb32c9ce5f2a22566482e4f7aef8b0f2d29cd9
Rafał Trzop <rafaltrzop@gmail.com>
2016-12-29 15:38:06 +0100

    Set up production server

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from checkers import app
+import sys
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    if len(sys.argv) == 2 and sys.argv[1] == '--production':
+        port = int(os.getenv('PORT'))
+        app.run(host='0.0.0.0', port=port, debug=False)
+    else:
+        app.run(host='0.0.0.0', debug=True)
