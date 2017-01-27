aa65285906e5f8f21ebc68f618098958cd220e9d
Rafał Trzop <rafaltrzop@gmail.com>
2017-01-28 00:48:47 +0100

    Add landing page with links to custom size of the board

--- checkers/checkers.py
+++ checkers/checkers.py
@@ -6,11 +6,14 @@ from .game import *
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/board/<int:board_size>')
-def index(board_size=8):
+def play(board_size=8):
     gameboard = Gameboard.build(board_size)
     board = gameboard.board
-    return render_template('index.html', board=board)
+    return render_template('play.html', board=board)
 
 @app.route('/move', methods=['POST'])
 def move():
