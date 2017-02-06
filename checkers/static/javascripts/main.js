2c32ee50ca13d540c506bf89305c811240fc8911
Rafał Trzop <rafaltrzop@gmail.com>
2017-02-06 19:49:13 +0100

    Add animation to moving pieces

--- checkers/static/javascripts/main.js
+++ checkers/static/javascripts/main.js
@@ -1,27 +1,42 @@
 $(document).ready(bind_events);
 
 function bind_events() {
-  var current_position = undefined;
-  var destination = undefined;
+  var current_position;
+  var destination;
+  var $selected_piece;
 
   var $black_squares = $('tr:nth-child(odd) td:nth-child(even), tr:nth-child(even) td:nth-child(odd)');
   $black_squares.click(function () {
     var found_piece_on_the_square = $(this).find('.board__piece').length;
     if (found_piece_on_the_square) {
+      $selected_piece = $(this).find('.board__piece').first();
       $('.board__square').removeClass('board__square--selected');
       $(this).addClass('board__square--selected');
 
       current_position = {
         x: $(this).data('x'),
-        y: $(this).data('y')
+        y: $(this).data('y'),
+        offset_left: $(this).offset().left,
+        offset_top: $(this).offset().top
       }
     } else if (current_position === undefined) {
       alert('Please choose a piece first.')
     } else {
       destination = {
         x: $(this).data('x'),
-        y: $(this).data('y')
+        y: $(this).data('y'),
+        offset_left: $(this).offset().left,
+        offset_top: $(this).offset().top
       }
+
+      var translate_x = destination.offset_left - current_position.offset_left;
+      var translate_y = destination.offset_top - current_position.offset_top;
+
+      $selected_piece.css({
+        transform: 'translate(' + translate_x + 'px, ' + translate_y + 'px)',
+        transition: 'transform .3s'
+      });
+
       var pieces = pieces_on_board();
 
       $.post('/move',
@@ -36,8 +51,10 @@ function bind_events() {
           last_move: GameConfig.last_move
         },
         function (data, status) {
-          $('body').html(data);
-          bind_events();
+          setTimeout(function () {
+            $('body').html(data);
+            bind_events();
+          }, 300);
         }
       );
     }
